c89cde70f383cc7be79e671d7e798aba8c15f75c
chan <baewonchan1@naver.com>
2020-07-25 18:49:21 +0900

    Add Calculator Test example

--- src/test/java/CustomCalculatorTest.java
+++ src/test/java/CustomCalculatorTest.java
@@ -1,4 +1,5 @@
 
+import javafx.scene.control.TableView;
 import org.junit.Before;
 import org.junit.Test;
 import static org.hamcrest.collection.IsEmptyCollection.empty;
@@ -17,9 +18,10 @@ public class CustomCalculatorTest {
     public void add() {
         customCalculator = new CustomCalculator();
         int result = customCalculator.add(10,15);
+        /* 이곳에 테스트 코드를 작성하세요. */
         assertTrue(result == 25);
         assertThat(result, is(25));
-        /* 이곳에 테스트 코드를 작성하세요. */
+        assertFalse(result != 25);
 //        System.out.println("result :: " + result);
     }
 
@@ -28,20 +30,22 @@ public class CustomCalculatorTest {
     public void subtract() {
         customCalculator = new CustomCalculator();
         int result = customCalculator.subtract(23,10);
-        //assertTrue(result == 13);
-        assertThat(result, is(13));
-        //assertFalse(result == 13);
         /* 이곳에 테스트 코드를 작성하세요. */
-        System.out.println("result :: " + result);
+        assertTrue(result == 13);
+        assertThat(result, is(13));
+        assertFalse(result == 13);
+        assertFalse(result != 13);
+        //System.out.println("result :: " + result);
     }
 
     @Test
     public void subtract2() {
         customCalculator = new CustomCalculator();
         int result = customCalculator.subtract(23,10);
-        assertFalse(result == 11);
         /* 이곳에 테스트 코드를 작성하세요. */
-        System.out.println("result :: " + result);
+        assertFalse(result == 11);
+        assertThat(result, is(13));
+        //System.out.println("result :: " + result);
     }
 
     //곱하기 테스트 작성
@@ -50,7 +54,9 @@ public class CustomCalculatorTest {
         customCalculator = new CustomCalculator();
         int result = customCalculator.multiply(5,9);
         /* 이곳에 테스트 코드를 작성하세요. */
-        System.out.println("result :: " + result);
+        assertThat(result, is(45));
+        assertTrue(result == 45);
+        //System.out.println("result :: " + result);
     }
 
     //나누기 테스트 작성
@@ -59,22 +65,29 @@ public class CustomCalculatorTest {
         customCalculator = new CustomCalculator();
         int result = customCalculator.divide(25,5);
         /* 이곳에 테스트 코드를 작성하세요. */
+        assertTrue(result == 12);
+        assertThat(result, is(5));
         System.out.println("result :: " + result);
     }
 
     @Test(timeout = 4000)
     public void timeInMethodTest() throws InterruptedException {
-        Thread.sleep(5000);
+        Thread.sleep(2000);
+        Thread.sleep(1998);
     }
 
+
     @Test(expected = IndexOutOfBoundsException.class)
-    public void testIsEmptyOutofBoundException(){
-        new ArrayList<Object>().get(0);
+    public void testIsEmptyIndexOutofBoundException(){
+       new ArrayList<Object>().get(0);
+       new ArrayList<Object>().get(2);
     }
+
 
     @Test
     public void isEmptyArray(){
         ArrayList<Object> myList = new ArrayList<>();
         assertThat(myList, is(empty()));
+        assertTrue(myList.isEmpty());
     }
 }
